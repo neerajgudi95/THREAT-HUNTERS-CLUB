d708689d5c45264803eef91a01f469a58109ec3d
neerajgudi95 <neeraj.gudi@outlook.com>
2023-02-02 23:17:16 +0530

    added our team component, profile card and css

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+const Login = () => (
+  <Formik
+    initialValues={{ email: "", password: "" }}
+    validate={(values) => {
+      const errors = {};
+      if (!values.email) {
+        errors.email = "Email is required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+      if (!values.password) {
+        errors.password = "Password is required";
+      }
+      return errors;
+    }}
+    onSubmit={(values, { setSubmitting }) => {
+      setTimeout(() => {
+        console.log(JSON.stringify(values, null, 2));
+        setSubmitting(false);
+      }, 400);
+    }}
+  >
+    {({ isSubmitting }) => (
+      <Form>
+        <Field type="email" name="email" placeholder="Email" />
+        <ErrorMessage name="email" component="div" />
+        <Field type="password" name="password" placeholder="Password" />
+        <ErrorMessage name="password" component="div" />
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+      </Form>
+    )}
+  </Formik>
+);
 
-export default Login
+export default Login;
--- src/components/our-team/OurTeam.jsx
+++ src/components/our-team/OurTeam.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import profile from "../../assets/profile.jpeg";
+import "./OurTeam.css";
 const OurTeam = () => {
-  return <div>OurTeam</div>;
+  return (
+    <div className="thc__ourTeam-wrapper section__padding">
+      <div className="thc__ourTeam-profile">
+        <div className="thc__ourTeam-profile__header">
+          <div className="thc__ourTeam-profile__header-nameInfo">
+            <h3>Nikhil Inganal</h3>
+            <p>-Senior Security Analyst</p>
+          </div>
+        </div>
+        <img src={profile} alt="Nikhil Inganal" />
+        <div className="thc__ourTeam-profile__about">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
+            debitis, dolor explicabo neque provident, earum, amet quibusdam et
+            tempore facilis quas harum esse magni voluptas illum rerum a dolore
+            deleniti.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default OurTeam;
